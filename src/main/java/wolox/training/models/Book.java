6cc69c23b8cb082bce3dce1a76f3c0b5308752a6
Assanchez <andrey.sanchez@wolox.co>
2021-05-24 13:55:57 -0500

    Update Book model fields null validation

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -1,5 +1,6 @@
 package wolox.training.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,22 +19,31 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
 
+    @Column(nullable = false)
     private String genre;
 
+    @Column(nullable = false)
     private String author;
 
+    @Column(nullable = false)
     private String image;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String subtitle;
 
+    @Column(nullable = false)
     private String publisher;
 
+    @Column(nullable = false)
     private String year;
 
+    @Column(nullable = false)
     private int pages;
-
+    
+    @Column(nullable = false)
     private String isbn;
 
 }
