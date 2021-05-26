1dee747669b14df85e0a0a6c637d31022bde4bed
Assanchez <andrey.sanchez@wolox.co>
2021-05-25 22:59:41 -0500

    Add user model and bookAlreadyOwnedException

--- src/main/java/wolox/training/enums/BookError.java
+++ src/main/java/wolox/training/enums/BookError.java
@@ -3,6 +3,7 @@ package wolox.training.enums;
 public enum BookError {
 
     WRONG_ID("Provide correct Book Id"),
+    ALREADY_OWNED("Book already owned"),
     BOOK_ID_MISMATCH("Book id mismatch");
 
     private String msg;
---
+++ src/main/java/wolox/training/models/User.java
@@ -0,0 +1,99 @@
+package wolox.training.models;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import wolox.training.exceptions.BookAlreadyOwnedException;
+
+/**
+ * Model to the table Users.
+ */
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private LocalDate birthdate;
+
+    @NotNull
+    @ManyToMany
+    private List<Book> books = new ArrayList<>();
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public List<Book> getBooks() {
+        return (List<Book>) Collections.unmodifiableList(books);
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    /**
+     * Add a book if not already exists
+     *
+     * @param book: The book to create to the user (Book)
+     */
+    public void addBook(Book book) {
+        if (books.contains(book)) {
+            throw new BookAlreadyOwnedException();
+        }
+        books.add(book);
+    }
+
+    /**
+     * Remove a book
+     *
+     * @param book: The book to be delete to the user (Book)
+     */
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+}
