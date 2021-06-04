6596854f1d785fd990c5a8a892ae83d0f4d2111d
Assanchez <andrey.sanchez@wolox.co>
2021-06-04 12:34:17 -0500

    remove unused imports

--- src/test/java/wolox/training/controllers/UserControllerTest.java
+++ src/test/java/wolox/training/controllers/UserControllerTest.java
@@ -1,6 +1,5 @@
 package wolox.training.controllers;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -17,16 +16,13 @@ import static wolox.training.builder.BookBuilder.book;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Example;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
